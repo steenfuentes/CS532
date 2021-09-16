018c9686f5056a57fb46755d59c5b8b341a0bcf3
devtamer <whiletam@gmail>
2021-09-15 19:29:41 -0700

    added .gitignore

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello_world():
-    return "Hello World"
+    request_method = request.method
+    return render_template('hello.html', request_method=request_method)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+# get and post requests
