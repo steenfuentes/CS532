09737bfc22fc21a09850e3f27ab2d71b54263cbe
seth <steenfuentes@gmail.com>
2021-09-25 21:36:48 -0700

    Basic routing done

--- health_app/forms.py
+++ health_app/forms.py
@@ -2,5 +2,5 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 
-class Form(FlaskForm):
-    #form code goes here
+# class Form(FlaskForm):
+#     #form code goes here
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from CalculatorApp import app
+from health_app import app
 
 if __name__ == '__main__':
     app.run(debug=True)
