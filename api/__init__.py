56e3b26346c2cd34c3b29ebbf7ed6df2dd9c60ed
seth <steenfuentes@gmail.com>
2021-10-06 21:02:40 -0700

    progress on API. Basic POST methods functioning for Patient model

--- api/__init__.py
+++ api/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from health_app.config import Config
+from .config import Config
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -14,7 +14,7 @@ def create_app(config_class=Config):
     db.init_app(app) 
     bcrypt.init_app(app)
 
-    from health_app.main.routes import main
-    app.register_blueprint(main)
+    from .models.patient.utils import patient
+    app.register_blueprint(patient)
 
     return app
--- api/health_app/models/patient/utils.py
+++
@@ -1,8 +0,0 @@
-
-from flask.blueprints import Blueprint
-
-from flask import render_template, url_for, flash, request, Blueprint
-
-patient = Blueprint('patient', __name__)
-
-# methods for accessing patient data and manipulating patient objects will go here
