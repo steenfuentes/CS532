62543281a514aaa4db4f0a4fd6169acc9b5b74f1
devtamer <whiletam@gmail>
2021-10-18 02:02:20 -0700

    restructured slightly in frontend, added components, next/router, and building out login/register pages

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c07fc15d2afd67b292b0"; }
+/******/ 		__webpack_require__.h = function() { return "9b192a968ee13cd63f95"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
