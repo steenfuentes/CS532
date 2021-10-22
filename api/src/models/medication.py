3bafda8ac3717bdda92c4cd72f5e8713765ad144
seth <steenfuentes@gmail.com>
2021-10-21 22:33:23 -0700

    MedicationModel inherits abstractmodel

--- api/src/models/medication.py
+++ api/src/models/medication.py
@@ -1,8 +1,9 @@
-# from enum import unique
-
-# from marshmallow.fields import Email
-from api import db
+"""
+Defines the model for a medication
+"""
 import enum
+
+from api import db
 from .abstractmodel import BaseModel, MetaBaseModel
 
 #enumerate marketing status
@@ -14,11 +15,11 @@ class mkt_status(enum.Enum):
 
 # just outlining the basic info needed that defines a medication 
 # a more elegent approach will establish patients as objects in the system
-class MedicationModel(db.Model):
-    __tablename__ = 'medication_reference'
+class MedicationModel(db.Model, BaseModel, metaclass=MetaBaseModel):
+    __tablename__ = 'medication'
     id = db.Column(db.Integer, primary_key=True)
     brand_name = db.Column(db.String(64), unique=False, nullable=False)
-    reference_standard = db.Column(db.String(64), unique=False, nullable=False)
+    reference_standard = db.Column(db.String(64), unique=False, nullable=False)g
     dosage_form = db.Column(db.String(64), unique=False, nullable=False)
     route = db.Column(db.String(64), unique=False, nullable=False)
     marketing_status = db.Column(db.Enum(mkt_status))
