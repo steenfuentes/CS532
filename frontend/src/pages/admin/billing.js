0b5673df29b389e7c8e4d22fac1083fd756903f7
seth <steenfuentes@gmail.com>
2021-11-13 21:21:05 -0800

    manually merged with 'tamer' branch. change SSL configuration to openssl-nodejs-1.0.5.

---
+++ frontend/src/pages/admin/billing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import GridContainer from '../../components/Grid/GridContainer'
+
+export default function billing() {
+    return (
+        <div>
+            <GridContainer>
+
+            </GridContainer>
+
+        </div>
+    )
+}
