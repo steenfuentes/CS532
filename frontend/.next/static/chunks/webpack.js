2f7d3453b1aa0955dfd20737770669aba01329a7
devtamer <whiletam@gmail.com>
2021-11-15 17:13:24 -0800

    updated ui patient

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a8bffd1d4a19c37f0f67"; }
+/******/ 		__webpack_require__.h = function() { return "98dd358bc52f4e9ab7ac"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/.next/static/webpack/webpack.68c1ee2fe371ad212e08.hot-update.js
+++ frontend/.next/static/webpack/webpack.68c1ee2fe371ad212e08.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "1398bdd00296587d838c"; }
+/******/ 	__webpack_require__.h = function() { return "7539458621a430d75078"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
