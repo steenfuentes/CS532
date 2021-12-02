7e6f7c94abf9705f5749e4d66d27fe0f4e73ad25
Samuel Schneider <sschneider2883.@sdsu.edu>
2021-12-01 18:42:04 -0800

    added the emplyee form and input components

--- frontend/src/components/forms/EquipmentForm.js
+++ frontend/src/components/forms/EquipmentForm.js
@@ -1,56 +1,121 @@
-import React, { useState } from 'react'
-import { makeStyles } from "@material-ui/core";
+import React, { useState, useEffect } from 'react'
+import { Grid, } from '@material-ui/core';
+import Controls from "../../components/controls/Controls";
+import { useForm, Form } from '../../components/useForm';
+import * as employeeService from "../../services/employeeService";
 
-export function useForm(initialFValues, validateOnChange = false, validate) {
 
+const ownedItems = [
+    { id: 'owned', title: 'Owned' },
+    { id: 'leased', title: 'Leased' },
+]
 
-    const [values, setValues] = useState(initialFValues);
-    const [errors, setErrors] = useState({});
+const departmentOptions = [
+    {id: 'MD', title: 'Medical Department'},
+    {id: 'LD', title: 'Lab Department'},
+    {id: 'PD', title: 'Pharmacy Department'},
+    {id: 'AD', title: 'Admin Department'},
 
-    const handleInputChange = e => {
-        const { name, value } = e.target
-        setValues({
-            ...values,
-            [name]: value
+]
+
+const initialFValues = {
+    equipmentId: 0,
+    equipmentType: '',
+    equipmentDescription: '',
+    department: '',
+    ownStatus: 'Owned',
+    purchaseDate: new Date()
+}
+
+export default function EmployeeForm() {
+
+    const validate = (fieldValues = values) => {
+        let temp = { ...errors }
+        if ('fullName' in fieldValues)
+            temp.fullName = fieldValues.fullName ? "" : "This field is required."
+        if ('email' in fieldValues)
+            temp.email = (/$^|.+@.+..+/).test(fieldValues.email) ? "" : "Email is not valid."
+        if ('mobile' in fieldValues)
+            temp.mobile = fieldValues.mobile.length > 9 ? "" : "Minimum 10 numbers required."
+        if ('departmentId' in fieldValues)
+            temp.departmentId = fieldValues.departmentId.length != 0 ? "" : "This field is required."
+        setErrors({
+            ...temp
         })
-        if (validateOnChange)
-            validate({ [name]: value })
+
+        if (fieldValues == values)
+            return Object.values(temp).every(x => x == "")
     }
 
-    const resetForm = () => {
-        setValues(initialFValues);
-        setErrors({})
-    }
-
-
-    return {
+    const {
         values,
         setValues,
         errors,
         setErrors,
         handleInputChange,
         resetForm
+    } = useForm(initialFValues, true, validate);
 
-    }
-}
-
-
-const useStyles = makeStyles(theme => ({
-    root: {
-        '& .MuiFormControl-root': {
-            width: '80%',
-            margin: theme.spacing(1)
+    const handleSubmit = e => {
+        e.preventDefault()
+        if (validate()){
+            employeeService.insertEmployee(values)
+            resetForm()
         }
     }
-}))
 
-export function Form(props) {
-
-    const classes = useStyles();
-    const { children, ...other } = props;
     return (
-        <form className={classes.root} autoComplete="off" {...other}>
-            {props.children}
-        </form>
+        <Form onSubmit={handleSubmit}>
+            <Grid container>
+                <Grid item xs={6}>
+                    <Controls.Input
+                        label="Equipment Type"
+                        name="equipmentType"
+                        value={values.equipmentType}
+                        onChange={handleInputChange}
+                        error={errors.equipmentType}
+                    />
+                    <Controls.Input
+                        label="Equipment Description"
+                        name="equipmentDescription"
+                        value={values.equipmentDescription}
+                        onChange={handleInputChange}
+                        error={errors.equipmentDescription}
+                    />
+                    <Controls.RadioGroup
+                        name="department"
+                        label="department"
+                        value={values.department}
+                        onChange={handleInputChange}
+                        items={departmentOptions}
+                    />
+
+                </Grid>
+                <Grid item xs={6}>
+                    <Controls.RadioGroup
+                        name="ownedStatus"
+                        label="Status"
+                        value={values.ownStatus}
+                        onChange={handleInputChange}
+                        items={ownedItems}
+                    />
+                    <Controls.DatePicker
+                        name="hireDate"
+                        label="Hire Date"
+                        value={values.hireDate}
+                        onChange={handleInputChange}
+                    />
+                    <div>
+                        <Controls.Button
+                            type="submit"
+                            text="Submit" />
+                        <Controls.Button
+                            text="Reset"
+                            color="default"
+                            onClick={resetForm} />
+                    </div>
+                </Grid>
+            </Grid>
+        </Form>
     )
 }
