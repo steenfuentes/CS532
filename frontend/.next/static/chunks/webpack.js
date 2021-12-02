b6260d86c380e1b05ef08a732f4dbf2f76470b38
Samuel Schneider <sschneider2883.@sdsu.edu>
2021-12-01 18:19:21 -0800

    added a folder for forms and created the UseForm component

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e146c902e9bd28d19fa3"; }
+/******/ 		__webpack_require__.h = function() { return "402d526f13f490b126fc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/src/pages/admin/equipment.js
+++ frontend/src/pages/admin/equipment.js
@@ -2,12 +2,19 @@ import React from 'react'
 import GridContainer from '../../components/Grid/GridContainer';
 import { Typography } from '@material-ui/core';
 import Admin from '../../layouts/Admin';
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import SearchBar from "material-ui-search-bar";
 
 function Equipment() {
     return (
         <GridContainer>
             <Typography variant="h4">Equipment Inventory and Maintenance</Typography>
-
         </GridContainer>
     )
 }
